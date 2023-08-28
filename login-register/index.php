891049a2ef041d2dcbe1c34cc6b03e5a563e5155
Theagns <markus.agnsgaard@hotmail.com>
2023-08-28 23:20:27 +0200

    Wrong index php

--- login-register/index.php
+++ login-register/index.php
@@ -2,67 +2,23 @@
 session_start();
 if (!isset($_SESSION["user"])) {
    header("Location: login.php");
-//    exit(); // Always exit after a redirect
 }
-
-require_once('database.php');
-$query = "SELECT * FROM users";
-$result = mysqli_query($database, $query); // Assuming you have defined $connection
-
 ?>
-
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" a href="css/bootstrap.css"/>
-    <title>View Records</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>User Dashboard</title>
 </head>
-<body class="bg-dark">
-
-        <div class="container">
-            <div class="row">
-                <div class="col m-auto">
-                    <div class="card mt-5">
-                        <table class="table table-bordered">
-                            <tr>
-                                <td> User ID </td>
-                                <td> User Name </td>
-                                <td> User Email </td>
-                                <td> User Age </td>
-                                <td> Edit  </td>
-                                <td> Delete </td>
-                            </tr>
-
-                            <?php 
-                                    
-                                    while($row=mysqli_fetch_assoc($result))
-                                    {
-                                        $UserID = $row['id'];
-                                        $UserName = $row['full_name'];
-                                        $UserEmail = $row['password'];
-                                        $UserAge = $row['role'];
-                            ?>
-                                    <tr>
-                                        <td><?php echo $UserID ?></td>
-                                        <td><?php echo $UserName ?></td>
-                                        <td><?php echo $UserEmail ?></td>
-                                        <td><?php echo $UserAge ?></td>
-                                        <td><a href="#" class="btn btn-pencil">Edit</a></td>
-                                        <td><a href="#" class="btn btn-danger">Delete</a></td>
-                                    </tr>        
-                            <?php 
-                                    }  
-                            ?>                                                                    
-                                   
-
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    
+<body>
+    <div class="container">
+        <h1>Welcome to Dashboard</h1>
+        <a href="logout.php" class="btn btn-warning">Logout</a>
+        <a href="getusers.php" class="btn btn-warning">Get users</a>
+    </div>
 </body>
 </html>
